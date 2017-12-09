016f799575a3ab3832ba55ade89d209d817fce3f
michael_zhang <499887880@qq.com>
2017-12-09 16:34:41 +0800

    third add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $(".user").mouseenter(function () {
         $(".user_info").slideDown();
@@ -10,6 +11,23 @@ $(document).ready(function () {
     var w=$(window).width();
     $("#myCarousel").height(w*570/1735);
 
+    $("input.form-control").focus(function(){
+        $(this).css("background-color","#FFFFCC");
+    });
+    $("input.form-control").blur(function(){
+        $(this).css("background-color","white");
+    });
+
+    $("input.form-control").keypress(function(event){
+        var keynum = (event.keyCode ? event.keyCode : event.which);
+        if(keynum == '13'){
+            $(this).css("background-color","white");
+            var data=this.value;
+            localStorage.setItem("search_data",data);
+            window.location.href="search.html";
+
+        }
+    });
 
 })
 
